0346c05ac9a08adf80c09f7ad00d8ff750c1ceba
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2021-05-27 22:45:42 +0700

    commit 2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import {Navbar, Nav, FormControl, Container, Form, Button} from 'react-bootstrap';
+import {BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import logo from './logo192.png';
 
+import Home from '../Pages/Home';
+import Contacts from '../Pages/Contacts';
+import About from '../Pages/About';
+import Blog from '../Pages/Blog';
 
 export default class Header extends Component {
     render() {
         return (
+            <>
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="/">
@@ -36,6 +42,16 @@ export default class Header extends Component {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/contacts" component={Contacts} />
+                    <Route exact path="/blog" component={Blog} />
+                </Switch>
+            </Router>
+</>
         )
     }
 }
